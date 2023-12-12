912572e67a2c4e4e5eafa2910978244c867f4ab0
Hassanaliuuq <106004498+Hassanaliuuq@users.noreply.github.com>
2023-12-12 19:17:20 +0500

    Update info.py

--- info.py
+++ info.py
@@ -15,7 +15,7 @@ def is_enabled(value, default):
 SESSION = environ.get('SESSION', 'Media_search')
 API_ID = int(environ.get('API_ID', '21845036'))
 API_HASH = environ.get('API_HASH', 'b9787357754c57417854ba8ca0d35129')
-BOT_TOKEN = environ.get('BOT_TOKEN', "6739185757:AAGUGHXveXbVPdM_ZHa8_QYoai1cCYAjScA")
+BOT_TOKEN = environ.get('BOT_TOKEN', "6657399606:AAHVrOp8gCGfXzE98Tj-FE4DmKWH-zWy8DE")
 
 # Bot settings
 CACHE_TIME = int(environ.get('CACHE_TIME', 300))
@@ -28,7 +28,7 @@ SPELL_IMG = environ.get("SPELL_IMG", "https://graph.org/file/c605a659444a78f828a
 
 # Admins, Channels & Users
 ADMINS = [int(admin) if id_pattern.search(admin) else admin for admin in environ.get('ADMINS', '5510849897').split()]
-CHANNELS = [int(ch) if id_pattern.search(ch) else ch for ch in environ.get('CHANNELS', '-1002036594476').split()]
+CHANNELS = [int(ch) if id_pattern.search(ch) else ch for ch in environ.get('CHANNELS', -1002014300927').split()]
 auth_users = [int(user) if id_pattern.search(user) else user for user in environ.get('AUTH_USERS', '').split()]
 AUTH_USERS = (auth_users + ADMINS) if auth_users else []
 PREMIUM_USER = [int(user) if id_pattern.search(user) else user for user in environ.get('PREMIUM_USER', '').split()]
@@ -43,7 +43,7 @@ SUPPORT_CHAT_ID = int(support_chat_id) if support_chat_id and id_pattern.search(
 NO_RESULTS_MSG = bool(environ.get("NO_RESULTS_MSG", False))
 
 # MongoDB information
-DATABASE_URI = environ.get('DATABASE_URI', "mongodb+srv://NOPE:NOPE@cluster0.shnvern.mongodb.net/?retryWrites=true&w=majority")
+DATABASE_URI = environ.get('DATABASE_URI', "mongodb+srv://NOW:NOW@cluster0.xkloxkw.mongodb.net/?retryWrites=true&w=majority")
 DATABASE_NAME = environ.get('DATABASE_NAME', "cluster0")
 COLLECTION_NAME = environ.get('COLLECTION_NAME', 'Telegram_files')
 
@@ -61,7 +61,7 @@ CHNL_LNK = environ.get('CHNL_LNK', 'https://t.me/hdlinks4uu')
 TUTORIAL = environ.get('TUTORIAL', 'https://t.me/starvibehowto/42')
 IS_TUTORIAL = bool(environ.get('IS_TUTORIAL', True))
 MSG_ALRT = environ.get('MSG_ALRT', 'God Bless you ❤️')
-LOG_CHANNEL = int(environ.get('LOG_CHANNEL', '-1002143964514'))
+LOG_CHANNEL = int(environ.get('LOG_CHANNEL', '-1002119311683'))
 SUPPORT_CHAT = environ.get('SUPPORT_CHAT', '-1001963382900')
 P_TTI_SHOW_OFF = is_enabled((environ.get('P_TTI_SHOW_OFF', "False")), False)
 IMDB = is_enabled((environ.get('IMDB', "True")), True)
